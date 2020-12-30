1cb8c710e625541f7b1718985e873b9c3020598a
裴博翔 <frankpei1992@gmail.com>
2020-12-30 13:08:14 +0800

    NDITable SDK 0.2.0

---
+++ Runtime/NDITableTouchFocus.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using xFrame.NDITable;
+using xFrame.NDITable.Unity.EventSystems;
+using xFrame.Unity;
+
+public class NDITableTouchFocus : XComponent {
+    private bool focus;
+
+#if UNITY_STANDALONE_WIN
+    private IntPtr hWnd = IntPtr.Zero;
+
+    [DllImport("user32.dll")]
+    private static extern uint GetActiveWindow();
+
+    [DllImport("user32.dll")]
+    public static extern IntPtr SetFocus(IntPtr hWnd);
+
+    void OnApplicationFocus(bool windowFocus) {
+        focus = windowFocus;
+    }
+
+    void Start() {
+        if (hWnd == IntPtr.Zero) {
+            hWnd = (IntPtr)GetActiveWindow();
+        }
+    }
+
+    void Update() {
+        if (!focus) {
+            if (NDITableInput.FingerCount() > 0
+            && NDITableInput.GetFingerDatas().Any(IsValidFinger)) {
+                SetFocus(hWnd);
+            }
+        }
+    }
+#endif
+
+    private bool IsValidFinger(FingerData data) {
+        return data.X >= 0.0f && data.X <= 1.0f && data.Y >= 0.0f && data.Y <= 1.0f;
+    }
+}
